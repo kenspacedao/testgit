935583e85c5d54d04a86c0ce9b9714d236103f11
Kenny <kenny@Kennys-Brand-New-MacBook-Pro.local>
2025-12-03 23:24:40 -0500

    L code

--- src/App.js
+++ src/App.js
@@ -8,20 +8,7 @@ import './firebase';
 function App() {
   return (
     <div className="app-structure-container">
-      <Helmet>
-        <meta
-          name="description"
-          content="TubeStamp is a free online tool that allows you to easily create timestamp links for YouTube videos. Share specific moments from videos with your friends and audience."
-        />
-        <meta
-          name="keywords"
-          content="YouTube, timestamp, links, video, sharing, free, online, tool"
-        />
-      </Helmet>
-      <LandingPage />
-    </div>
-  );
-}
+      <H
 
-export default App;
+        export default App;
 //j
